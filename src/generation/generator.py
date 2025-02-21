cf1a5deef6808d566c1c86a2cf21162b3a96504c
taufiqdp <topexdw@gmail.com>
2025-02-21 14:42:18 +0700

    Refactor generate_answer function to accept llm_model as a parameter and update prompt_builder logic for user queries

--- src/generation/generator.py
+++ src/generation/generator.py
@@ -1,10 +1,4 @@
-from models.llm import OllamaLLM, TogetherLLM
-
-# llm_model = TogetherLLM()
-llm_model = OllamaLLM()
-
-
-def generate_answer(prompt, stream=True):
+def generate_answer(prompt, llm_model, stream=True):
     """Generates an answer from the LLM based on the prompt."""
     response = llm_model.generate_response(prompt, stream=stream)
     return response
--- src/generation/prompt_builder.py
+++ src/generation/prompt_builder.py
@@ -4,7 +4,7 @@ from utils.utils import clean_text
 def build_prompt(context, question):
     """Builds the prompt for the LLM."""
     template = """You are an expert at summarizing and resolving contradictory information. Answer the following question based on the provided context.
-If the question is not relevant to the context, please answer "The question is not relevant to the context."
+If user asks basic questions, forget about the context and answer the question directly.
 
 Context:
 
