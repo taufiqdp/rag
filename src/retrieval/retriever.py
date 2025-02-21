70668113365b380f610967ee15e3c9cb5f5aa8e9
taufiqdp <topexdw@gmail.com>
2025-02-21 14:41:48 +0700

    Refactor retrieve_relevant_chunks function to accept embedding model and Qdrant database as parameters

--- src/retrieval/retriever.py
+++ src/retrieval/retriever.py
@@ -1,12 +1,4 @@
-from models.embedding import AzureOpenAIEmbeddings, HFEmbeddings
-from models.qdrant_client import QdrantDB
-
-# embedding_model = AzureOpenAIEmbeddings()
-embedding_model = HFEmbeddings()
-qdrant_db = QdrantDB()
-
-
-def retrieve_relevant_chunks(question, limit=5):
+def retrieve_relevant_chunks(question, embedding_model, qdrant_db, limit=5):
     """Retrieves relevant chunks from the Qdrant database based on a question."""
     question_embedding = embedding_model.get_embedding(question)
     contexts = qdrant_db.query(query_vector=question_embedding, limit=limit)
